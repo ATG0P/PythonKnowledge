c27396fc18ccdfbe7c4448eb2916d2d3cb4c07b9
ATG0P <dahiyaarya70@gmail.com>
2025-11-14 19:51:31 +0530

    Revision of basics of Numpy Done!

--- DSA_PY/roughwork/r5.py
+++ DSA_PY/roughwork/r5.py
@@ -8,3 +8,11 @@ print(a.ndim)
 print(b)
 print(b.ndim)
 print(b.shape)
+c =np.array([
+            [[1,2,3],[4,5,6],[7,8,9]],
+            [[10,11,12],[13,14,15],[16,17,18]],
+            [[19,20,21],[22,23,24],[25,26,27]]
+            ])
+print(c.shape)
+print(c[2][1][0])
+print(c[0,0,1])
