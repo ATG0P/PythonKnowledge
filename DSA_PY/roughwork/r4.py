3d2150b739a126da270b1ba8e972549023ec87d4
ATG0P <dahiyaarya70@gmail.com>
2025-11-14 19:36:02 +0530

    rough work files added for revision

---
+++ DSA_PY/roughwork/r4.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number: "))
+print("Even numbers from 1 to ",a," are :- ")
+for i in range(1,a+1):
+    if(i%2==0):
+        print(i)
+    else:
+        continue
---
+++ DSA_PY/roughwork/r5.py
@@ -0,0 +1,6 @@
+import numpy as np
+a = np.array([1,2,3,4])
+a*=2
+print(a)
+print(type(a))
+print(a.ndim)
