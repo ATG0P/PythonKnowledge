ae1a90152fa82c41ce582b9db41ca67d458ec875
ATG0P <dahiyaarya70@gmail.com>
2025-11-15 08:36:48 +0530

    Finally revised arthmetic in Numpy

--- DSA_PY/roughwork/r6.py
+++ DSA_PY/roughwork/r6.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 arr1 = np.array([1,2,3])
 arr1+=100
@@ -7,3 +8,9 @@ print(arr1)
 arr2=np.array([[1,2,3],[4,5,6],[7,8,9]])
 arr2[0,0]*=100
 print(arr2)
+arr3=np.array([4.6,2.23,9.8])
+print(np.sqrt(arr3))
+print(np.square(arr3))
+print(np.ceil(arr3))
+print(np.floor(arr3))
+print(np.pi)
---
+++ DSA_PY/roughwork/r7.py
@@ -0,0 +1,17 @@
+import numpy as np
+arr1=np.array([1,2,3])
+arr2=np.array([2,4,9])
+print(arr1+arr2)
+print(arr2-arr1)
+print(arr2/arr1)
+print(arr2*arr1)
+print(arr2%arr1)
+print(arr2**arr1)
+
+print(arr1>=arr2)
+print(arr1==arr2)
+print(arr1<=3)
+print(arr2>=3)
+arr1[arr1<=3]=0
+arr2[arr2>=3]=0
+print(arr1,arr2)
