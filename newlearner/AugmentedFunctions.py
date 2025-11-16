a85bc24fe65f4486434dab0dd871ce651b873c08
ATG0P <dahiyaarya70@gmail.com>
2025-11-16 10:23:17 +0530

    I learned basic Augemented Functions!

---
+++ newlearner/AugmentedFunctions.py
@@ -0,0 +1,14 @@
+import numpy as np 
+array = np.array([  [1,2,3,4,5],
+                    [6,7,8,9,10]])
+print(np.sum(array))
+print(np.mean(array))
+print(np.std(array))#standard deviation
+print(np.var(array))#variance
+print(np.min(array))
+print(np.max(array))
+print(np.argmin(array))#index of minimum number
+print(np.argmax(array))#index of maximum number
+
+print(np.sum(array, axis=0))#returns an array of sum of all indexes=0 
+print(np.sum(array, axis=np.argmin(array)))#basically, index=0
